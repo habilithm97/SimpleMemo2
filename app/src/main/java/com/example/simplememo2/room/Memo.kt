6b0a4701ceb698f7a0c72b0b61e51e91a1c38fc1
habilithm97 <habinkim97@gmail.com>
2024-12-07 13:42:42 +0900

    Room Database

--- app/src/main/java/com/example/simplememo2/room/Memo.kt
+++ app/src/main/java/com/example/simplememo2/room/Memo.kt
@@ -7,7 +7,7 @@ import androidx.room.PrimaryKey
  -DB의 테이블을 나타내는 데이터 클래스
  -테이블 구조와 1:1 매핑, DB 관리 단순화
 */
-@Entity(tableName = "memo")
+@Entity(tableName = "memo_table")
 data class Memo(
     @PrimaryKey(autoGenerate = true)
     var id: Int = 0,
---
+++ app/src/main/java/com/example/simplememo2/room/MemoDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.simplememo2.room
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+// Room은 데이터베이스 구현체를 자동 생성하므로, 추상 클래스로 선언
+@Database(entities = [Memo::class], version = 1, exportSchema = true)
+abstract class MemoDatabase : RoomDatabase() {
+    abstract fun memoDao(): MemoDao
+
+    companion object {
+        /* Volatile
+         -여러 스레드에서 최신 값을 읽을 수 있도록 보장
+         -멀티 스레드 환경에서 일관성을 유지하도록 처리 */
+        @Volatile
+        private var INSTANCE: MemoDatabase? = null
+        private const val DB_NAME = "memo_database"
+
+        fun getInstance(context: Context) : MemoDatabase {
+            // synchronized : 멀티 스레드 환경에서 여러 스레드가 동시에 생성하지 못하도록 동기화
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MemoDatabase::class.java,
+                    DB_NAME
+                ).build()
+                INSTANCE = instance // 새로 생성한 instance를 INSTANCE에 저장
+                instance
+            }
+        }
+    }
+}
